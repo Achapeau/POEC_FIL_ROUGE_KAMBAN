66e34ea029b48a95177a54404a745570f239c8eb
Robin <dev.robininizan@gmail.com>
2024-04-23 13:11:51 +0200

    add search bar in sidebar

--- TaskWave/src/app/Components/sidebar/sidebar.component.ts
+++ TaskWave/src/app/Components/sidebar/sidebar.component.ts
@@ -1,10 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  Router,
-  RouterLink,
-  RouterOutlet,
-} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../Service/user.service';
@@ -13,22 +8,16 @@ import { ProjectService } from '../../Service/project.service';
 import { User, Project } from '../../Model/model';
 import { ModalComponent } from '../../modal/modal.component';
 import { ModalNewProjectComponent } from '../../modal/modal-new-project/modal-new-project.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, MatIconModule, CommonModule, RouterOutlet, ModalNewProjectComponent],
+  imports: [RouterLink, MatIconModule, CommonModule, RouterOutlet, ModalNewProjectComponent, ModalComponent, FormsModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  constructor(
-    public userService: UserService,
-    public authService: AuthService,
-    public projectService: ProjectService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
   userLog!: string;
   isActive: boolean = true;
   myUser!: Partial<User>;
@@ -36,18 +25,38 @@ export class SidebarComponent implements OnInit {
   projects!: Project[];
   myProjects!: Project[];
   isModalOpen = false;
+  searchQuery: string = ''; // Ajout pour la gestion de la recherche
+
+  constructor(
+    public userService: UserService,
+    public authService: AuthService,
+    public projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.authService.userData$.subscribe((myUser) => {
       this.myUser = myUser;
     });
     this.projects = this.projectService.convertProjectIdsToProjects(this.myUser.projectsIds!);
+    this.getProject(); // Assurez-vous que la liste des projets est mise à jour au démarrage
   }
 
   getProject() {
-    this.myProjects = this.projects.filter((project) =>
-      project.userIds?.includes(this.myUser.id!)
-    );
+    if (this.searchQuery) {
+      this.myProjects = this.projects.filter((project) =>
+        project.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    } else {
+      this.myProjects = this.projects.filter((project) =>
+        project.userIds?.includes(this.myUser.id!)
+      );
+    }
+  }
+
+  onSearchChange(): void {
+    this.getProject(); // Mise à jour des projets filtrés lors de chaque modification de recherche
   }
 
   toggle() {
@@ -70,6 +79,7 @@ export class SidebarComponent implements OnInit {
   closeModal() {
     this.isModalOpen = false;
   }
+
   logout() {
     this.userService.disconnectUser();
   }
